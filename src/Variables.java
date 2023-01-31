821c91e5ca2f9372e152c212f1c22fe24ed116f4
Lucas Patrick <lucas.patrick.lsilva@gmail.com>
2023-01-30 22:53:33 -0300

    Java operations

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void variables(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int sub = num1 - num2;
+        int div = num1 / num2;
+        int multi = num1 * num2;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(div);
+        System.out.println(multi);
+    }
+}
